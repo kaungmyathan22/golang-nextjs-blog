27621a053fa34fcdb3ea289b1e1b363c5a5ecdde
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-26 09:47:57 +0630

    chore(): code config setup

---
+++ server/cmd/config.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	logger "github.com/kaungmyathan22/golang-blog/cmd/common"
+	"github.com/spf13/viper"
+)
+
+var ConfigInstance Config
+
+func (app *Application) BootstrapApp() {
+	logger.Init()
+	app.LoadConfig()
+}
+
+func (app *Application) LoadConfig() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	envFile := fmt.Sprintf("%s/.env", dir)
+	viper.SetConfigFile(envFile)
+	viper.ReadInConfig()
+	viper.AutomaticEnv()
+	if err := viper.Unmarshal(&ConfigInstance); err != nil {
+		logger.Fatal(err.Error())
+	}
+
+}
