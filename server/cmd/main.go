cc1123194beb210204f9a86c0a7dc4b2165a0ea3
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-03-26 09:48:41 +0630

    chore(): config struct

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type Config struct {
+	PORT int
+	ENV  string
+}
+
+type Application struct {
+	config Config
+	wg     sync.WaitGroup
+}
 
 func main() {
 	fmt.Println("Hola amigos!!!")
