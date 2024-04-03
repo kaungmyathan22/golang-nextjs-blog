14281200e2bc55d3806de596903b08bcf0c8e841
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-04-03 23:15:51 +0630

    feat(): customized valdiation response

--- server/cmd/middlewares/validation_middleware.go
+++ server/cmd/middlewares/validation_middleware.go
@@ -1,7 +1,9 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
+	"reflect"
 	"sync"
 
 	"github.com/gin-gonic/gin"
@@ -26,7 +28,7 @@ func ValidateRequest(validationStruct interface{}) gin.HandlerFunc {
 
 		if contentType == "application/json" {
 			if err := validateJSON(c, validationStruct); err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				c.JSON(http.StatusBadRequest, gin.H{"error": formatValidationErrors(err, validationStruct)})
 				c.Abort()
 				return
 			}
@@ -35,8 +37,11 @@ func ValidateRequest(validationStruct interface{}) gin.HandlerFunc {
 	}
 }
 
-// Function to validate the JSON body
 func validateJSON(c *gin.Context, validationStruct interface{}) error {
+	if validationStruct == nil {
+		return nil
+	}
+
 	if err := c.ShouldBindJSON(validationStruct); err != nil {
 		return err
 	}
@@ -46,4 +51,17 @@ func validateJSON(c *gin.Context, validationStruct interface{}) error {
 	}
 
 	return nil
+}
+
+func formatValidationErrors(err error, validationStruct interface{}) map[string]string {
+	errors := make(map[string]string)
+	if validationErrs, ok := err.(validator.ValidationErrors); ok {
+		t := reflect.TypeOf(validationStruct).Elem()
+		for _, err := range validationErrs {
+			field, _ := t.FieldByName(err.Field())
+			fieldName := field.Tag.Get("json")
+			errors[fieldName] = fmt.Sprintf("Field validation for '%s' failed on the '%s' tag", fieldName, err.Tag())
+		}
+	}
+	return errors
 }
--- server/cmd/routes/auth_routes.go
+++ server/cmd/routes/auth_routes.go
@@ -3,13 +3,15 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kaungmyathan22/golang-blog/cmd/handlers"
+	"github.com/kaungmyathan22/golang-blog/cmd/middlewares"
+	"github.com/kaungmyathan22/golang-blog/cmd/models"
 )
 
 func AuthRoutes(v1Routes *gin.RouterGroup) {
 	handlers := &handlers.AuthHandler{}
 	router := v1Routes.Group("/authentication")
 	// public routes
-	router.POST("/login", handlers.LoginHandler)
+	router.POST("/login", middlewares.ValidateRequest(&models.LoginPayload{}), handlers.LoginHandler)
 	router.POST("/register", handlers.RegisterHandler)
 	// private routes
 }
