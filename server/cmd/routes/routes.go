873009102749d3844b546e9af6293e2e1af6cd4f
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-04-07 12:53:20 +0630

    feat(): health check endpoint added

--- server/cmd/routes/routes.go
+++ server/cmd/routes/routes.go
@@ -1,11 +1,19 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes(r *gin.Engine) {
 	v1Routes := r.Group("/api/v1")
 	// setup auth routes
+	v1Routes.GET("/monitoring/ping", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, map[string]any{
+			"message": "pong",
+		})
+		return
+	})
 	AuthRoutes(v1Routes)
 }
