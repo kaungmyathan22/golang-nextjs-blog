d4bf6ce4371d3f0b1df76cadc6718652615a7898
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-16 21:55:17 +0630

    feat(): new sql script added

--- server/query.sql
+++ server/query.sql
@@ -2,6 +2,10 @@
 SELECT * FROM users
 WHERE id = $1 LIMIT 1;
 
+-- name: GetUserbyEmail :one
+SELECT * FROM users
+WHERE email = $1 LIMIT 1;
+
 -- name: GetUsers :many
 SELECT * FROM users
 ORDER BY id;
