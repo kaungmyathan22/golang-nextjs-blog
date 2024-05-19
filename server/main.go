104e7f72ba48f8da4106f48ed52757126c5171c7
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-19 21:51:13 +0630

    update(): app bootstrapping

--- server/main.go
+++ server/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kaungmyathan22/golang-nextjs-blog/app/config"
+	"github.com/kaungmyathan22/golang-nextjs-blog/app/routes"
+)
+
 func main() {
+	// load env
+	config.LoadConfig()
+	// setup route
+	r := gin.Default()
+	r.Use(gin.Logger())
+	routes.SetupRoute(r)
+	r.Run(config.ConfigInstance.PORT)
 
 }
