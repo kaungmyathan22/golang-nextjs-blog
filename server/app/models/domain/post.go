ae6507dd5d3d129534fb733d313021abbbd37a2e
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-20 14:13:56 +0630

    feat(domain): post domain structs added

---
+++ server/app/models/domain/post.go
@@ -0,0 +1,22 @@
+package domain
+
+import (
+	"time"
+)
+
+type Category struct {
+	ID        int64
+	Title     string
+	Createdat time.Time
+	Updatedat time.Time
+}
+
+type Post struct {
+	ID         int64
+	Title      string
+	Content    string
+	Createdat  time.Time
+	Updatedat  time.Time
+	Authorid   int
+	Categoryid int
+}
