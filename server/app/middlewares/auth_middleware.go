f590b1049864f626b47f07d66bc786e027a4ea13
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-22 22:34:50 +0630

    update(): logging added

--- server/app/middlewares/auth_middleware.go
+++ server/app/middlewares/auth_middleware.go
@@ -1,18 +1,22 @@
 package middlewares
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt"
-	"github.com/kaungmyathan22/golang-nextjs-blog/app/config"
+	"github.com/kaungmyathan22/golang-nextjs-blog/app/database"
+	"github.com/kaungmyathan22/golang-nextjs-blog/app/logger"
 	"github.com/kaungmyathan22/golang-nextjs-blog/app/models/apis"
+	models "github.com/kaungmyathan22/golang-nextjs-blog/app/models/domain"
+	jwt "github.com/kaungmyathan22/golang-nextjs-blog/app/utils"
 )
 
 func IsAuthenticated() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		tokenString := ctx.GetHeader("Authorization")
-
+		logger.Info(fmt.Sprintf("token string is %s", tokenString))
 		if tokenString == "" {
 			response := apis.APIResponse{
 				Status:  http.StatusUnauthorized,
@@ -23,12 +27,10 @@ func IsAuthenticated() gin.HandlerFunc {
 			ctx.Abort()
 			return
 		}
-
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			return []byte(config.ConfigInstance.JWT_TOKEN_SECRET), nil
-		})
-
-		if err != nil || !token.Valid {
+		token := strings.Split(tokenString, "Bearer ")[1]
+		claims, err := jwt.ValidateJwtAuthenticationToken(token)
+		if err != nil {
+			logger.Error(err.Error())
 			response := apis.APIResponse{
 				Status:  http.StatusUnauthorized,
 				Message: "Unauthorized",
@@ -39,6 +41,19 @@ func IsAuthenticated() gin.HandlerFunc {
 			return
 		}
 
+		var user *models.User
+		result := database.DB.First(&user, claims.ID)
+		if result.Error != nil {
+			response := apis.APIResponse{
+				Status:  http.StatusUnauthorized,
+				Message: "Unauthorized",
+				Data:    nil,
+			}
+			ctx.JSON(http.StatusUnauthorized, response)
+			ctx.Abort()
+			return
+		}
+		ctx.Set("user", user)
 		ctx.Next()
 	}
 
