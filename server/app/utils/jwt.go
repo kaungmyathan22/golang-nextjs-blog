7e5d5895b441c42b29e0d47bc63fac01f6533015
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-22 22:35:25 +0630

    update(): reusable jwt functions added

--- server/app/utils/jwt.go
+++ server/app/utils/jwt.go
@@ -5,19 +5,34 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
+	"github.com/golang-jwt/jwt"
 	"github.com/kaungmyathan22/golang-nextjs-blog/app/config"
 )
 
 type contextKey string
+type JwtCustomClaims struct {
+	Sub string `json:"sub"`
+	Iat int64  `json:"iat"`
+	Exp int64  `json:"exp"`
+	jwt.StandardClaims
+}
 
 const UserKey contextKey = "userID"
 
 func SignJwtAuthenticationToken(sub int) (string, error) {
 	fmt.Println(config.ConfigInstance.JWT_TOKEN_SECRET)
-	expiration := time.Second * time.Duration(config.ConfigInstance.JWTExpirationInSeconds)
-	claims := jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiration)),
+	expiration := time.Now().Add(1 * time.Hour)
+	claims := JwtCustomClaims{
+		Sub: strconv.Itoa(sub),
+		Iat: time.Now().Unix(),
+		Exp: expiration.Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(secretKey)
+
+	claims := JwtCustomClaims{
+		Exp:       jwt.NewNumericDate(time.Now().Add(expiration)),
 		ID:        strconv.Itoa(sub),
 		IssuedAt:  jwt.NewNumericDate(time.Now()),
 		NotBefore: jwt.NewNumericDate(time.Now()),
@@ -28,21 +43,21 @@ func SignJwtAuthenticationToken(sub int) (string, error) {
 	return tokenString, err
 }
 
-func ValidateJwtAuthenticationToken(tokenString string) (*jwt.MapClaims, error) {
+func ValidateJwtAuthenticationToken(tokenString string) (*jwt.RegisteredClaims, error) {
+	fmt.Println(tokenString)
+	fmt.Println("tokenString")
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method %v", token.Header["alg"])
-		}
 		return []byte(config.ConfigInstance.JWT_TOKEN_SECRET), nil
 	})
-
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error parsing token: %v", err)
 	}
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		return &claims, nil
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
 	}
-	return nil, fmt.Errorf("invalid token")
-
+	claims, ok := token.Claims.(jwt.RegisteredClaims)
+	if !ok {
+		return nil, fmt.Errorf("error while parsing token to register claims")
+	}
+	return &claims, err
 }
