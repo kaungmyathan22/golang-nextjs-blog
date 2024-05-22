50c9301da8bdcc283ab2909b7fd9aae9a89dce62
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-22 22:35:08 +0630

    update(): middleware added to /me route

--- server/app/routes/route.go
+++ server/app/routes/route.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kaungmyathan22/golang-nextjs-blog/app/controllers"
+	"github.com/kaungmyathan22/golang-nextjs-blog/app/middlewares"
 )
 
 func SetupRoute(r *gin.Engine) {
@@ -17,7 +18,7 @@ func SetupRoute(r *gin.Engine) {
 	authRoute.POST("/forgot-password", authHandler.ForgotPassword)
 	authRoute.POST("/reset-password", authHandler.ResetPassword)
 
-	authRoute.POST("/me", authHandler.Me)
+	authRoute.GET("/me", middlewares.IsAuthenticated(), authHandler.Me)
 
 	apiV1Group.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
