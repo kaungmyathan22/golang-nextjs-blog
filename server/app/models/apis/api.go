9cfa7c1579943baef6ff3004bd5dc4fc8520f2c6
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-24 23:28:22 +0630

    update(): resusable api response created

--- server/app/models/apis/api.go
+++ server/app/models/apis/api.go
@@ -1,7 +1,34 @@
 package apis
 
+import "net/http"
+
 type APIResponse struct {
 	Status  int         `json:"status"`
 	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
+}
+
+var UnauthorizedResponse = APIResponse{
+	Message: "error",
+	Status:  http.StatusUnauthorized,
+	Data:    nil,
+}
+
+var InternalServerErrorResponse = APIResponse{
+	Message: "error",
+	Status:  http.StatusInternalServerError,
+	Data:    nil,
+}
+var BadRequestResponse = APIResponse{
+	Message: "error",
+	Status:  http.StatusBadRequest,
+	Data:    nil,
+}
+
+func GetStatusAcceptedResponse(data any) *APIResponse {
+	return &APIResponse{
+		Message: "success",
+		Status:  http.StatusOK,
+		Data:    data,
+	}
 }
