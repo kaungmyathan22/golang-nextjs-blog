0aaaa573fac50044d7f5c953c6d5e50cd2d5d8bb
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-26 10:03:49 +0630

    chore(): remove account model

--- server/app/models/domain/user.go
+++ server/app/models/domain/user.go
@@ -2,24 +2,13 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        int64  `json:"id" gorm:"primarykey"`
-	Name      string `json:"name"`
-	Password  string `json:"-"`
-	Email     string `gorm:"unique" json:"email"`
-	Accounts  []Account
+	ID        int64     `json:"id" gorm:"primarykey"`
+	Name      string    `json:"name"`
+	Password  string    `json:"-"`
+	Email     string    `gorm:"unique" json:"email"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
-}
-
-type Account struct {
-	ID          int64
-	Lastloginat time.Time
-	UserID      int64
-	User        User `gorm:"constraint:OnDelete:CASCADE;"`
-	gorm.Model
 }
