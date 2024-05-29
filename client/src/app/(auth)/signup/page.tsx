ec6e0f5c6d436e21b6c252c5763e3eb4ccb993ae
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-29 22:36:42 +0630

    feat(client): signup ui added

---
+++ client/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Link from "next/link";
+
+import Client from "@/components/shared/Client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Signup() {
+  return (
+    <Client>
+      <div className="w-screen h-screen grid place-content-center">
+        <Card className="mx-auto max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-xl">Sign Up</CardTitle>
+            <CardDescription>
+              Enter your information to create an account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-4">
+              <div className="grid gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="first-name">Full name</Label>
+                  <Input id="full-name" placeholder="Eric Han" required />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" />
+              </div>
+              <Button type="submit" className="w-full">
+                Create an account
+              </Button>
+              {/* <Button variant="outline" className="w-full">
+                Sign up with GitHub
+              </Button> */}
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link href="/signin" className="underline">
+                Sign in
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Client>
+  );
+}
