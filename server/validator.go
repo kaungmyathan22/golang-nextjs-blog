5c24eb795509e5ff182f21cb2da7d45636163160
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-06-02 23:46:04 +0630

    update(): comment added

--- server/validator.go
+++ server/validator.go
@@ -2,6 +2,7 @@ package main
 
 import "regexp"
 
+// password validator
 func sixToEightDigitAlphanumericPasswordValidator(password string) bool {
 	re := regexp.MustCompile(`^[a-zA-Z0-9]{6,8}$`)
 	return re.MatchString(password)
