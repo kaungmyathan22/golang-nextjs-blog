67dcd6a00979a17922e85182f89cdc0b23e90d89
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-06-07 14:52:50 +0630

    update(): forogt password link added

--- client/src/app/(auth)/signin/page.tsx
+++ client/src/app/(auth)/signin/page.tsx
@@ -39,7 +39,7 @@ export default function Signin() {
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                   <Link
-                    href="#"
+                    href="/forgot-password"
                     className="ml-auto inline-block text-sm underline"
                   >
                     Forgot your password?
