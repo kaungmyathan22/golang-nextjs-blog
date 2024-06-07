47ba16e20e37217e35449c63ae20eb1c24a8f13f
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-06-07 14:50:33 +0630

    feat(): home page configured

--- client/src/app/(auth)/signin/page.tsx
+++ client/src/app/(auth)/signin/page.tsx
@@ -50,9 +50,6 @@ export default function Signin() {
               <Button type="submit" className="w-full">
                 Login
               </Button>
-              <Button variant="outline" className="w-full">
-                Login with Google
-              </Button>
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
